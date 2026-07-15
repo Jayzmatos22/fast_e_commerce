8a5fd939aad0c83892acfc5f3b83013f0783b180
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-15 16:48:17 -0300

    feat: dtos dummy: body product

---
+++ backend/src/main/java/com/ecommerce/backend/dtos/DummyProduct.java
@@ -0,0 +1,30 @@
+package com.ecommerce.backend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DummyProduct(
+        Integer id,
+        String title,
+        String description,
+        String category,
+        BigDecimal price,
+        BigDecimal discountPercentage,
+        BigDecimal rating,
+        Integer stock,
+        List<String> tags,
+        String brand,
+        String sku,
+        BigDecimal weight,
+        DummyDimensions dimensions,
+        String warrantyInformation,
+        String shippingInformation,
+        String availabilityStatus,
+        String returnPolicy,
+        Integer minimumOrderQuantity,
+        List<String> images,
+        String thumbnail
+) {}
