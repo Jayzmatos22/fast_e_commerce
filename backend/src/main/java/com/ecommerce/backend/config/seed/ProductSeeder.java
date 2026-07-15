ee6afab5e05feddea3411a89b4519f37f42cecd6
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-15 17:01:55 -0300

    feat: seed for insert produtcs

---
+++ backend/src/main/java/com/ecommerce/backend/config/seed/ProductSeeder.java
@@ -0,0 +1,43 @@
+package com.ecommerce.backend.config.seed;
+
+import com.ecommerce.backend.dtos.DummyProductsResponse;
+import com.ecommerce.backend.model.Product;
+import com.ecommerce.backend.model.ProductMapper;
+import com.ecommerce.backend.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+@RequiredArgsConstructor
+public class ProductSeeder implements CommandLineRunner {
+    private final ProductRepository productRepository;
+    private final RestTemplate restTemplate;
+
+    private static final String DUMMY_URL = "https://dummyjson.com/products?limit=194";
+
+    @Override
+    public void run(String... args) {
+        if (productRepository.count() > 0) {
+            System.out.println("Produtos já existentes, seed ignorado.");
+            return;
+        }
+
+        DummyProductsResponse response = restTemplate.getForObject(DUMMY_URL, DummyProductsResponse.class);
+
+        if (response == null || response.products() == null) {
+            System.out.println("Falha ao buscar produtos da DummyJSON.");
+            return;
+        }
+
+        for (var dto : response.products()) {
+            Product produto = ProductMapper.toEntity(dto);
+            productRepository.save(produto);
+        }
+
+        System.out.println("Seed concluído: " + response.products().size() + " produtos inseridos.");
+    }
+}
+
