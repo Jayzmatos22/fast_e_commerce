1d997a93a559f6569fba5e29eaca24488e8ae75b
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-15 17:37:04 -0300

    refactor: alter client for api

--- backend/src/main/java/com/ecommerce/backend/config/seed/ProductSeeder.java
+++ backend/src/main/java/com/ecommerce/backend/config/seed/ProductSeeder.java
@@ -7,6 +7,7 @@ import com.ecommerce.backend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -14,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class ProductSeeder implements CommandLineRunner {
     private final ProductRepository productRepository;
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     private static final String DUMMY_URL = "https://dummyjson.com/products?limit=194";
 
@@ -25,7 +26,10 @@ public class ProductSeeder implements CommandLineRunner {
             return;
         }
 
-        DummyProductsResponse response = restTemplate.getForObject(DUMMY_URL, DummyProductsResponse.class);
+        DummyProductsResponse response = restClient.get()
+                .uri(DUMMY_URL)
+                .retrieve()
+                .body(DummyProductsResponse.class);
 
         if (response == null || response.products() == null) {
             System.out.println("Falha ao buscar produtos da DummyJSON.");
