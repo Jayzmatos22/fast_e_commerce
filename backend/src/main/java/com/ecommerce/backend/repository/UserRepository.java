cd41bf28a3d89ab1fc76771a1c364aee8cb1d45b
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-16 14:52:47 -0300

    feat: query findbyemail

--- backend/src/main/java/com/ecommerce/backend/repository/UserRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.ecommerce.backend.repository;
 import com.ecommerce.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    // <TipoDaEntidade, TipoDoID>
+    Optional<User> findByEmail(String email);
 }
