954ba20c316702ff1340b588e789e7dd05ad072c
Jailton Santos <jailtonmatos200@gmail.com>
2026-07-20 10:46:57 -0300

    refactor: change annotation for validation

--- backend/src/main/java/com/ecommerce/backend/dtos/user/UserUpdateRequest.java
+++ backend/src/main/java/com/ecommerce/backend/dtos/user/UserUpdateRequest.java
@@ -1,10 +1,21 @@
 package com.ecommerce.backend.dtos.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record UserUpdateRequest(
+
+        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
         String name,
+
+        @Size(min = 11, max = 20, message = "O telefone deve ter entre 11 e 20 caracteres!")
         String phoneUser,
+
+        @Past(message = "A data de nascimento deve ser uma data passada")
+        @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDate birthDate
 ) {
 }
